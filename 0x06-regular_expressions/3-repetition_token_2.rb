1d5e482c922bbf87a6530206e00313d67034312e
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-11-09 23:22:08 +0000

    Repetition Token #2

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+m1 = ("#{ARGV}").scan(/h(?>bt{1,})n/) #ARGV le los argumentos de la linea de comandos de bash
+m2 = m1.map(&:inspect).join('') #lo convierte de matriz a string
+m2.gsub! /"/, ''   #Quita las comillas dobles
+puts m2
