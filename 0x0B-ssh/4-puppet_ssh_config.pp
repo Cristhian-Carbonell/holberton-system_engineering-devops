cf8450e1406e2d62dfde0789d15b2bb1a4f5799b
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-12-09 13:56:24 +0000

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,10 +1,12 @@
 # Client configuration file (w/ Puppet)
 include stalib
-file_line { 'Use_private_key':
+
+file_line { 'private key':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/holberton',
 }
-file_line { 'Refuse_password':
+
+file_line { 'password':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
