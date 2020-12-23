6182ecc472bd1478ff90df7e1b9de03bc7e70812
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-12-23 21:08:04 +0000

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Add a custom HTTP header with Puppet
 exec {'http':
-    commnad => 'sudo apt-get update && sudo apt-get install nginx -y &&
-                custom_header="\\\tadd_header X-Served-By \$HOSTNAME:\n"
-                && sudo sed -i "17i $custom_header" /etc/nginx/nginx.conf
-                sudo service nginx restart',
+commnad => 'sudo apt-get update && sudo apt-get install nginx -y &&
+            custom_header="\\\tadd_header X-Served-By \$HOSTNAME:\n"
+            && sudo sed -i "17i $custom_header" /etc/nginx/nginx.conf
+            sudo service nginx restart',
 }
