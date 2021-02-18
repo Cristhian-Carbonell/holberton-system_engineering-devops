b2ed2e87fdc36d2a474c99a79fcee051681ef52f
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-02-18 15:53:08 -0500

    puppet file to change server settings for access

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # we use the SED command to change class-wp-locale.phpp to class-wp-locale.php in wp-settings.php file
 exec { 'fixer':
-    command => 'sed -i "s|class-wp-locale.phpp|class-wp-locale.php|g" /var/www/html/wp-settings.php',
+    command  => 'sed -i "s|class-wp-locale.phpp|class-wp-locale.php|g" /var/www/html/wp-settings.php',
     provider => shell,
 }
